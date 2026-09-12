3e70c2e24ae05faea851d08c3faef37e7b75bb94
RishikeshKumarYadav872 <rishikeshyadav0027@gmail.com>
2026-09-12 11:08:50 +0530

    Time: 3 ms (26.84%) | Memory: 15.5 MB (14.64%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<vector<int>>& ans,vector<int>& v,int idx){
+    void helper(vector<int>& nums, vector<vector<int>>& ans,vector<int>v,int idx){
         if(idx==nums.size()){ans.push_back(v); return;}
 
         helper(nums, ans,v,idx+1);
         v.push_back(nums[idx]);
         helper(nums, ans,v,idx+1);
-        v.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>v;
